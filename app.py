98d26af1c8fc166cbb88967912a8a350e2c858e2
luck rpg <ferreiraagerlan@gmail.com>
2025-03-20 12:49:09 -0300

    senai

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route('/<name>')
 def hello_name(name):
     return f'ola, {name}!'
+
 
 @app.route('/soma/<num1>+<num2>')
 def soma(num1, num2):
     return f'ola, {num1} + {num2} = {num1 + num2}'
 
+
 @app.route('/somas/<int:num1>/<int:num2>')
 def somas(num1, num2):
     return f'ola, {num1} + {num2} = {num1 + num2}'
 
+@app.route('/menos/<int:num1>/<int:num2>')
+def menos(num1, num2):
+    return f'ola, {num1} - {num2} = {num1 - num2}'
+
+@app.route('/menos/<int:num1>/<int:num2>')
+def multplica(num1, num2):
+    return f'ola, {num1} * {num2} = {num1 * num2}'
+
+@app.route('/menos/<int:num1>/<int:num2>')
+def divisao(num1, num2):
+    return f'ola, {num1} / {num2} = {num1 / num2}'
+
+# esse if aqui em baixo não pode sair deste local tem que ser o último
 if __name__ == '__main__':
     app.run(debug=True)
