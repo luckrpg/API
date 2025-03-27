54d1df68a1f83a90bf5a6f2e243cf4119f1c3305
luck rpg <ferreiraagerlan@gmail.com>
2025-03-27 10:40:05 -0300

    senai

--- data.py
+++ data.py
@@ -1,29 +1,49 @@
+from datetime import datetime, date
 from flask import Flask, jsonify
-import datetime
-from datetime import datetime
+from flask_pydantic_spec import FlaskPydanticSpec
+
 app = Flask(__name__)
+spec = FlaskPydanticSpec('flask',
+                          title = 'First API - Senai',
+                          version = '1.0.0',)
+spec.register(app)
+tempo = datetime.now()
 
-@app.route('/dia/')
-def dia():
-    agora = datetime.now()
-    agora = agora.strftime('%Y-%m-%d | %H:%M:%S')
-    return jsonify({"agora": agora}), 200
-@app.route('/dia_atua/<data>')
-def dias_valores(data):
-        data_entrada = datetime.strptime(data, '%Y-%m-%d')
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+@app.route('/<dia>/<mes>/<ano>')
+def data(dia, mes, ano):
+
+    try:
+        dia = int(dia)
+        mes = int(mes)
+        ano = int(ano)
+        temporecebido = datetime(ano, mes, dia)
         data_atual = datetime.now()
+        data_composta = data_atual.strftime('%d/%m/%Y')
+
+        if temporecebido > data_atual:
+            situacao = 'futuro'
+        elif temporecebido < data_atual:
+            situacao = 'passado'
+        else:
+            situacao = 'presente'
+
+        dias_diferenca = abs(temporecebido - data_atual).days
+        meses_diferenca = dias_diferenca // 30
+        anos_diferenca = dias_diferenca // 365.25
+
+        return jsonify({'situacao': situacao,
+                        "data_inserida": temporecebido,
+                        "data_atual": data_composta,
+                        "dias de diferenca": dias_diferenca,
+                        "meses de diferenca": meses_diferenca,
+                        "anos de diferenca": anos_diferenca})
+    except TypeError:
+        return jsonify({'situacao': 'erro'})
 
 
-        days_difference = abs((data_atual - data_entrada).days)
-
-
-        years_difference = abs(data_atual.year - data_entrada.year)
-        months_difference = abs(data_atual.month - data_entrada.month)
-
-        total_months_difference = years_difference * 12 + months_difference
-
-        return {
-            "dias_de_differença": days_difference,
-            "meses_de_diferença": total_months_difference}
 if __name__ == '__main__':
     app.run(debug=True)
